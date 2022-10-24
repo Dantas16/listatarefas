377dc1be2ad0d25761bbbbde07ef8f170fe72774
Gutemberg Dantas de Oliveira Alencar <gutemberg.alencar@serpro.gov.br>
2022-10-24 17:51:54 -0300

    Add google analytics

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,7 +4,10 @@ import "./home.css";
 function Home() {
     return(
         <div className="home">
-            <h3 className="title">Bem vindes</h3>
+            <h3 className="title">
+                Bem vindes
+            </h3>
+
             <p>Escolha uma opção:</p>
             <Link className="link" to="/cadastrar">Cadastrar</Link>
             <Link className="link" to="/login">Login</Link>
